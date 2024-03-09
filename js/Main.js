e806052df4ccbff7e467199006e785a86d7ab18b
nuwan-dev <nuwanc.email@gmail.com>
2024-03-09 19:27:52 +0530

    c

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,10 @@
 // sroll color change
-
 const tpNavMenu = document.querySelector(".hoolix-top-nav-container");
+const mbNavMenu = document.querySelector(".hoolix-mb-nav-container");
 window.addEventListener("scroll", () => {
   window.scrollY > 1
-    ? tpNavMenu.classList.add("top-nav-scroll-active")
-    : tpNavMenu.classList.remove("top-nav-scroll-active");
+    ? (tpNavMenu.classList.add("top-nav-scroll-active"),
+      mbNavMenu.classList.add("mb-nav-scroll-active"))
+    : (tpNavMenu.classList.remove("top-nav-scroll-active"),
+      mbNavMenu.classList.remove("mb-nav-scroll-active"));
 });
