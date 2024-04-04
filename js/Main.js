3e440fa838757d59fb33e7f361d06948ea402562
nuwan-dev <nuwanc.email@gmail.com>
2024-04-04 18:25:25 +0530

    c

--- js/Main.js
+++ js/Main.js
@@ -60,3 +60,37 @@ $(".our-services-slider").slick({
     },
   ],
 });
+
+$(".our-work-slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  autoplay: true,
+  autoplaySpeed: 1250,
+  slidesToScroll: 1,
+  dots: false,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
